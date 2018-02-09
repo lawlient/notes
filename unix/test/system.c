83012c667dedd63a1c779c71a4f9f7e6422eca46
lawlient <jiuanx2011@163.com>
2018-02-09 12:58:16 +0800

    upload my files first time

---
+++ unix/test/system.c
@@ -0,0 +1,32 @@
+#include "../apue.h"
+#include <sys/wait.h>
+
+int main(void) {
+  int status;
+
+    if ((status = system("date")) < 0) {
+      err_sys("system() error");
+    }
+
+    pr_exit(status);
+
+    if ((status = system("nosuchcommand")) < 0) {
+      err_sys("system() error");
+    }
+    
+    pr_exit(status);
+
+    if ((status = system("who; exit 44")) < 0) {
+      err_sys("system() error");
+    }
+
+    pr_exit(status);
+
+    if ((status = system("change-wallpaper.sh; exit 111")) < 0) {
+      err_sys("system() error");
+    }
+
+    pr_exit(status);
+
+    exit(0);
+}
