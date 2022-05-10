debb56a514ed1ea2d7398c496d5c8bd7cc5d3fd2
lawlient <jiuanx2011@163.com>
2022-05-10 23:52:30 +0800

    upload send-email bash script

---
+++ bin/send-email.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sender="jiuanx2011@163.com"                     #邮件发送者
+stmp_addr="smtp.163.com"                        #服务器地址
+topic="test send email script from xujiuan"     #邮件主题
+receiver="jiuanx2011@163.com"                   #邮件接收地址，使用逗号分隔
+
+#邮件内容
+msg="
+你好！\n
+    我是测试邮件。\n
+祝你一切顺利！\n
+
+Date: 2022-05-10\n
+Addr: 702\n
+"
+
+sendEmail -u $topic -f $sender -s $stmp_addr -t $receiver -m $msg -xu $sender -xp $1
+
