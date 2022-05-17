c02178b70439f29ac10fe5e9eb356468d8ca8637
lawlient <jiuanx2011@163.com>
2022-05-18 00:10:09 +0800

    upload more ncurses demo

--- ncurses/test.c
+++ ncurses/test.c
@@ -3,6 +3,7 @@
 int main() {
   initscr(); // init ncurses mode, include MACRO like LINES COLS and so on
   noecho();  // not echo input on screen
+  keypad(stdscr, true);  // enable Fn Key input
   attron(A_BLINK); // enable blink
   move(0, COLS/2); // move cursor to 0,COLS/2
   int row = LINES/2;
@@ -10,15 +11,20 @@ int main() {
   mvaddstr(row, col, "Press 'q' to exit!"); // show string at (row, col)
   refresh(); // show text on screen now
 
-  char c = ' ';
+  int c = ' ';
   while ((c = getch() /* get input from keyboard */) != 'q') {
+    if (c == KEY_F(1)) {
+      clear(); mvaddstr(row, col, "Fn 1");
+    } else {
       switch (c) {
         case '1':clear(); mvaddstr(row, col, "Mode 1"); break;
         case '2':clear(); mvaddstr(row, col, "Mode 2"); break;
         case '3':clear(); mvaddstr(row, col, "Mode 3"); break;
+        case KEY_F(1):clear(); mvaddstr(row, col, "Fn 1"); break;
         default: mvaddstr(row, col, "Press 'q' to exit!"); break;
       }
-      refresh();
+    }
+    refresh();
   }
   
   endwin(); // exit ncurses mode
