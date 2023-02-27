e0654425f03a7a7d960eece27dd2a0baef0266bd
lawlient <jiuanx2011@163.com>
2023-02-27 21:07:03 +0800

    summary sort algorithm

--- c/algorithm/sort/strategy/heapsort.c
+++ c/algorithm/sort/strategy/heapsort.c
@@ -4,16 +4,17 @@
 #define L(p) (2 * (p) + 1)
 #define R(p) (2 * (p) + 2)
 
-static int aux(int a[], int n, int h);
-static int buildheap(int a[], int n);
+static int down(int a[], int n, int h);
 
 
 int heap_sort(int a[], int n) {
-    buildheap(a, n);
+    for (int h = n / 2; h >= 0; h--) {
+        down(a, n, h);
+    }
 
     while (--n) {
         SWAP(a[n], a[0]);
-        aux(a, n, 0);
+        down(a, n, 0);
     }
 
     return 0;
@@ -22,7 +23,7 @@ int heap_sort(int a[], int n) {
 /* 假设a满足大顶堆，h为需要调整的节点
  * 迭代向下转移hole，直至叶子节点
  */
-int aux(int a[], int n, int h) {
+int down(int a[], int n, int h) {
     int l = L(h), r = R(h);
     while (l < n && r < n) {
         int m = a[l] < a[r] ? r : l;
@@ -39,13 +40,3 @@ int aux(int a[], int n, int h) {
     return 0;
 }
 
-/* 从最后一个非叶子节点向下调整，以此调整至第一个节点（堆顶）
- * 即完成堆的构建
- */
-int buildheap(int a[], int n) {
-    int h;
-    for (h = n / 2; h >= 0; h--) {
-        aux(a, n, h);
-    }
-    return 0;
-}
---
+++ c/algorithm/sort/strategy/shellsort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/* 希尔增量：
+ *
+ *     1. shell增量：每次折半降低至1，时间复杂度依旧O(N)
+ *     2. Hibbard: 2 ^ k - 1，O(N ^ 1.5)
+ *     3. Sedgewick： 9 * 4 ^ i - 9 * 2 ^ i + 1
+ * */
+
+int shellsort(int a[], int n) {
+    // TODO
+    return 0;
+}
